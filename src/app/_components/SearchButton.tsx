0810fc91c4ed7eacf02aec62e49c53bb35c97760
dango-the-world <z.rohaso@gmail.com>
2025-03-05 23:47:22 +0900

    feat: モーダルのスタイルを改善し、検索入力フィールドにプレースホルダーを追加

--- src/app/_components/SearchButton.tsx
+++ src/app/_components/SearchButton.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { Box, Input, Modal, useDisclosure } from "@yamada-ui/react";
+import {
+  Box,
+  Input,
+  Modal,
+  ModalOverlay,
+  useDisclosure,
+} from "@yamada-ui/react";
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 
@@ -19,10 +25,23 @@ export const SearchButton = () => {
         <FaSearch size="24px" />
       </Box>
 
-      <Modal open={open} onClose={onClose} placement={"top"} size={"6xl"}>
-        <Box w={"70%"} h={"400px"}>
-          <Input />
-        </Box>
+      <Modal
+        open={open}
+        onClose={onClose}
+        placement={"top"}
+        size={"6xl"}
+        bgColor={"#111827"}
+      >
+        <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
+        <Input
+          padding={"10px"}
+          bgColor={"#111827"}
+          placeholder="ワードで検索"
+          borderColor="#680c62"
+          sx={{
+            _placeholder: { color: "#555" },
+          }}
+        />
       </Modal>
     </Box>
   );
